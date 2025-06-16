dd653cbc681d849f2b866bf600dd0e827b554c38
asyaD <asyadudnik@hotmail.com>
2025-06-16 14:17:25 +0100

    added folder flatMap

--- src/main/java/org/tests/streams/FlatMapper.java
+++ src/main/java/org/tests/streams/FlatMapper.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class FlatMapper {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Product chair = new Product("Chair", 150);
         Product table = new Product("Table", 350);
         Product pencil = new Product("Pencil", 2);
@@ -18,10 +21,12 @@ public class FlatMapper {
                         .stream())
                 .map(Product::name)
                 .forEach(System.out::println);
+        Order.examplesFlatMap();
 
     }
+
     @Getter
-    static class Order  {
+    static class Order {
         List<Product> products;
         double totalPrice;
 
@@ -31,16 +36,32 @@ public class FlatMapper {
         }
 
         private void calculateTotalPrice(List<Product> products) {
-            for(Product product : products) {
+            for (Product product : products) {
                 totalPrice += product.price();
             }
+        }
 
+        private static void examplesFlatMap() {
+            Stream<String> strings = Stream.of("10000000000,20000000000", "30000000000");
+            LongStream longStream = strings.flatMapToLong(s -> Arrays.stream(s.split(","))
+                    .mapToLong(Long::parseLong));
 
+            longStream.forEach(System.out::println);
+//=======================================================================
+            Stream<String> strings1 = Stream.of("1.1,2.2", "3.3,4.4");
+            DoubleStream doubleStream = strings1.flatMapToDouble(s -> Arrays.stream(s.split(","))
+                    .mapToDouble(Double::parseDouble));
 
+            doubleStream.forEach(System.out::println);
+            //================================================
+            Stream.of(1, 2, 3).<String>mapMulti((number, consumer) -> {
+                consumer.accept(number + "a");
+                consumer.accept(number + "b");
+            }).forEach(System.out::println);
         }
 
     }
 
-    record Product (String name, int price) {
+    record Product(String name, int price) {
     }
 }
